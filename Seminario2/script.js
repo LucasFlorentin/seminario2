f8d75f5b848693a6d7713c60a7873ecf15dccc88
LucasFlorentin <130504631+LucasFlorentin@users.noreply.github.com>
2024-11-14 15:34:09 -0300

    actualizacion

--- Seminario2/script.js
+++ Seminario2/script.js
@@ -1,28 +1,4 @@
-// Lista de usuarios permitidos con correos y contraseñas correspondientes
-// const usuariosPermitidos = [
-//     { email: "lucas@gmail.com", password: "lucas123" },
-//     { email: "javier@gmail.com", password: "javier123" },
-//     { email: "test@gmail.com", password: "test123" }
-// ];
 
-// // Evento de envío de formulario
-// document.getElementById("loginForm").addEventListener("submit", function(event) {
-//     event.preventDefault();
-
-//     const email = document.getElementById("email").value;
-//     const password = document.getElementById("password").value;
-
-//     // Función para verificar las credenciales
-//     const usuarioValido = usuariosPermitidos.find(usuario => usuario.email === email && usuario.password === password);
-
-//     if (usuarioValido) {
-//         // Redirigir a la página de productos si las credenciales son correctas
-//         window.location.href = "productos.html";
-//     } else {
-//         // Mostrar un mensaje de error si las credenciales no son válidas
-//         alert("Correo electrónico o contraseña incorrectos. Intente nuevamente.");
-//     }
-// });
 
 //  Lista de usuarios permitidos con correos y contraseñas correspondientes
  const usuariosPermitidos = [
